54ade912f774285fd4d302d502716720a1637fd9
Rick Love <rick@toldpro.com>
2016-12-30 10:40:12 -0600

    Add coveralls

--- karma.conf.js
+++ karma.conf.js
@@ -11,7 +11,7 @@ module.exports = function (config) {
       "**/*.ts": ["karma-typescript"]
     },
 
-    reporters: ["progress", "karma-typescript"],
+    reporters: ["progress", "karma-typescript", "coveralls"],
 
     karmaTypescriptConfig: {
       reports: {
