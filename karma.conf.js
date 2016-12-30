ffdc8a32cd06a6682b49f21756274e477f653f5c
Rick Love <rick@toldpro.com>
2016-12-30 08:05:11 -0600

    FIX Travis Chrome

--- karma.conf.js
+++ karma.conf.js
@@ -1,38 +1,44 @@
-module.exports = function(config) {
-    config.set({
+module.exports = function (config) {
+  config.set({
 
-        frameworks: ["jasmine", "karma-typescript"],
+    frameworks: ["jasmine", "karma-typescript"],
 
-        files: [
-            { pattern: "src/**/*.ts" }
-        ],
+    files: [{
+      pattern: "src/**/*.ts"
+    }],
 
-        preprocessors: {
-            "**/*.ts": ["karma-typescript"]
-        },
+    preprocessors: {
+      "**/*.ts": ["karma-typescript"]
+    },
 
-        reporters: ["progress", "karma-typescript"],
+    reporters: ["progress", "karma-typescript"],
 
-        // Uncomment below if you want the default html
-        // coverage report + a summary on the console
-        /*
-        karmaTypescriptConfig: {
-            reports:
-            {
-                "html": "coverage",
-                "text-summary": "" // destination "" will redirect output to the console
-            }
-        },
-        //*/
+    // Uncomment below if you want the default html
+    // coverage report + a summary on the console
+    /*
+    karmaTypescriptConfig: {
+        reports:
+        {
+            "html": "coverage",
+            "text-summary": "" // destination "" will redirect output to the console
+        }
+    },
+    //*/
 
-        // Uncomment below if you want to disable code coverage
-        // instrumentation during debugging of tests
-        /*
-        karmaTypescriptConfig: {
-            disableCodeCoverageInstrumentation: true
-        },
-        //*/
+    // Uncomment below if you want to disable code coverage
+    // instrumentation during debugging of tests
+    /*
+    karmaTypescriptConfig: {
+        disableCodeCoverageInstrumentation: true
+    },
+    //*/
 
-        browsers: ["Chrome"]
-    });
+    browsers: ["Chrome"],
+    customLaunchers: {
+      Chrome_travis_ci: {
+        base: "Chrome",
+        flags: ["--no-sandbox"]
+      }
+    }
+  });
 };
